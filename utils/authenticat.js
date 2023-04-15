16b0c9119007ad91bc41fa47e50536d05e10ea2b
Serhii Markov <goo.komalel@gmail.com>
2023-04-15 07:23:16 +0300

    login&verification done!

--- utils/authenticat.js
+++ utils/authenticat.js
@@ -8,16 +8,16 @@ const authenticat = async (req, res, next) => {
     const { authorization = ""} = req.headers;
     const [bearer, token] = authorization.split(" ");
     // console.log("ITS ME authorization!!!", authorization);
-    if (bearer !== "Bearer") return next(HttpError(401));
+    if (bearer !== "Bearer") return next(HttpError(401,"Not authorized"));
     try {
         const { id } = jwt.verify(token, SECRET_KEY);
         // console.log("id!!!",id);
         const user = await User.findById(id);
-        if (!user || !user.token) return next(HttpError(401)); 
+        if (!user || !user.token) return next(HttpError(401,"Not authorized")); 
         req.user = user;                      // створ. в об-ті req власт-ь user і записуємо дані з const user!
         next();
     } catch  {
-        next(HttpError(401));
+        next(HttpError(401,"Not authorized"));
     }
 }
 
